91706a1bd36478d04b17abff9c225312a907d7c8
daebwae <stefan@nerdocracy.org>
2015-03-18 23:52:49 +0100

    Initial commit - HyperLogLog in early stages of dev... lots of tweaks missing

---
+++ lib/space/HyperLogLog.py
@@ -0,0 +1,31 @@
+import sys
+__author__ = 'Stefan Zapf'
+
+
+class HyperLogLog(object):
+    """ Implementation of HyperLogLog
+    currently in dev, just a basic implementation
+    """
+    def __init__(self):
+        self.max_zeroes = 0
+
+    def add(self, item):
+        h = hash(item)
+        binary = bin(h)
+
+        zeros = 0
+        for bit in reversed(binary):
+            if bit == "1":
+                break
+            zeros += 1
+
+        self.max_zeroes = max(self.max_zeroes, zeros)
+
+    def compute(self):
+        return 2 ** (self.max_zeroes + 1)
+
+if __name__ == "__main__":
+    hll = HyperLogLog()
+    for arg in sys.argv[1:]:
+        hll.add(arg)
+    print hll.compute()
